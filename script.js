fa631ddbc4a31231c5ee8fabe53e77298e6e071d
WavyOre0 <bobgotgames@gmail.com>
2026-01-03 09:33:13 -0600

    home page is 75% done

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  menu.classList.add('open');
+  icon.classList.add('open');
+}
